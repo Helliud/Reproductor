be7d4d9172910b35787bf4ab7c7edd2004f0d56c
Javier Eliud Lizarraga Martin Del Campo <jelmdc@hotmail.com>
2019-02-22 11:24:49 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Reproductor
+{
+    public partial class MainWindow : Window
+    {
+        AudioFileReader reader;
+        //Nuestra comunicacion con la tarjeta de sonido
+        WaveOutEvent output;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void btnElegirArchivo_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+                txtRutaArchivo.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            reader = new AudioFileReader(txtRutaArchivo.Text);
+        }
+    }
+
+}
